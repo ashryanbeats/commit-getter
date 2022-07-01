22531a846e138e7b8d528c76d8fa470394258909
Ash Ryan Arnwine <accounts@ashryan.io>
2022-07-01 02:49:17 +0000

    Getting repo commits.

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 import "dotenv/config.js";
-import { Octokit, App } from "octokit";
+import { Octokit } from "octokit";
 
+// Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
+const octokit = new Octokit({ auth: process.env.GH_ACCESS_TOKEN });
 
-console.log(process.env.JOYFUN);
+const {
+  data: commits
+} = await octokit.request('GET /repos/{owner}/{repo}/commits', {
+  owner: 'ashryanbeats',
+  repo: 'commit-getter'
+});
 
+console.log("Here are the URLs for %s commits:", commits.length);
+
+commits.map(commit => console.log(`\n${commit.html_url}`));
