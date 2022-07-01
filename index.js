26b797549050f004c6c25794648ddfb49d1ab102
Ash Ryan Arnwine <accounts@ashryan.io>
2022-07-01 19:27:15 +0000

    Appending commit URLs to page as bookmarks.

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ import { Client as Notion } from "@notionhq/client";
 const octokit = new Octokit({ auth: process.env.GH_ACCESS_TOKEN });
 const notion = new Notion({ auth: process.env.NO_SECRET });
 
-const databaseId = process.env.NO_COMMIT_GETTER_DB_ID;
-
 const {
   data: commits
 } = await octokit.request('GET /repos/{owner}/{repo}/commits', {
@@ -15,33 +13,24 @@ const {
 });
 
 console.log("Here are the URLs for %s commits:", commits.length);
-
 commits.map(commit => console.log(`\n${commit.html_url}`));
 
-async function addCommit(commit) {
-  try {
-    const response = await notion.pages.create({
-      parent: { database_id: databaseId },
-      properties: {
-        title: {
-          title: [
-            {
-              "text": {
-                "content": commit.html_url
-              }
-            }
-          ]
-        }
-      },
-    });
+async function addCommits(commits) {
+  const children = commits.map(commit => {
+    return {
+      object: "block",
+      type: "bookmark",
+      bookmark: {
+        url: commit.html_url
+      }
+    }
+  });
 
-    console.log(response);
-    console.log("Success! Entry added.");
-  } catch (error) {
-    console.error(error.body);
-  }
-}
+  const response = await notion.blocks.children.append({
+    block_id: process.env.NO_COMMIT_GETTER_PAGE_ID,
+    children
+  });
+  console.dir(response, {depth: null});
+};
 
-commits.map(commit => {
-  addCommit(commit);
-});
+addCommits(commits);
