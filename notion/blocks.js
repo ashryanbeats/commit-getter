a44d5b81326b35aa1ad1f42c1532c488d4427103
Ash Ryan Arnwine <accounts@ashryan.io>
2022-07-07 02:15:27 +0000

    Moved blocks logic to own module.

---
+++ notion/blocks.js
@@ -0,0 +1,141 @@
+import { getUserFromDict } from "./getUserFromDict.js";
+
+const createNewChildren = (commits, children) => {
+  const commitBlocks = createCommitBlocks(commits);
+  const insertIdx = findInsertIdx(children);
+  children.results.splice(insertIdx, 0, ...commitBlocks);
+
+  return children;
+}
+
+const createCommitBlocks = (commits) => {
+  const commitBlocks = commits
+  .map((commit) => {
+    const commitPar = createCommitPar(commit);
+    const commitBookmark = createCommitBookmark(commit);
+
+    return [commitPar, commitBookmark];
+  })
+
+  return commitBlocks.flat();
+}
+
+const createCommitPar = (commit) => {
+  return {
+    type: "paragraph",
+    paragraph: {
+      rich_text: [
+        {
+          type: "text",
+          text: {
+            content: `Commit `,
+          },
+        },
+        {
+          type: "text",
+          annotations: {
+            code: true,
+          },
+          text: {
+            content: `${commit.sha.substring(0, 7)}`,
+          },
+        },
+        {
+          type: "text",
+          annotations: {
+            bold: true,
+          },
+          text: {
+            content: `: ${commit.commit.message}`,
+          },
+        },
+      ],
+    },
+  };
+};
+
+const createCommitBookmark = (commit) => {
+  const commitBookmark = {
+    object: "block",
+    type: "bookmark",
+    bookmark: {
+      url: commit.html_url,
+      caption: [
+        {
+          type: "text",
+          text: {
+            content: `By `,
+          },
+        },
+        {
+          type: "text",
+          text: {
+            content: ` (`,
+          },
+        },
+        {
+          type: "text",
+          text: {
+            content: `${commit.author.login}`,
+            link: {
+              type: "url",
+              url: `${commit.author.html_url}`,
+            },
+          },
+        },
+        {
+          type: "text",
+          text: {
+            content: `) on `,
+          },
+        },
+        {
+          type: "mention",
+          mention: {
+            date: {
+              start: commit.commit.author.date,
+            },
+          },
+        },
+      ],
+    },
+  };
+
+  return _insertUserMentionOrName(commit, commitBookmark);
+};
+
+const _insertUserMentionOrName = (commit, commitBookmark) => {
+  const user = getUserFromDict(commit);
+
+  user
+    ? commitBookmark.bookmark.caption.splice(1, 0, {
+        type: "mention",
+        mention: {
+          user: {
+            id: user.notionUserId,
+          },
+        },
+      })
+    : commitBookmark.bookmark.caption.splice(1, 0, {
+        type: "text",
+        text: {
+          content: commit.commit.author.name,
+        },
+      });
+
+  return commitBookmark;
+};
+
+const findInsertIdx = (blocks) => {
+  const targetBlockIdx = blocks.results.findIndex((block) => {
+    return (
+      block.type === "heading_2" &&
+      block.heading_2.rich_text[0].plain_text === "Docs"
+    );
+  });
+
+  return targetBlockIdx + 1;
+}
+
+
+export { createNewChildren };
--- notion/index.js
+++ notion/index.js
@@ -1,133 +1,22 @@
 import { Client as Notion } from "@notionhq/client";
+import { createNewChildren } from "./blocks.js";
 import { getChildren, archiveChildren, appendChildren } from "./notionApi.js";
-import { getUserFromDict } from "./getUserFromDict.js";
 import { exitWithError } from "../lib/index.js";
 const notion = new Notion({ auth: process.env.NO_SECRET });
 
 async function addCommits(commits) {
-  const children = commits
-    .map((commit) => {
-      const user = getUserFromDict(commit);
-
-      const paragraph = {
-        type: "paragraph",
-        paragraph: {
-          rich_text: [
-            {
-              type: "text",
-              text: {
-                content: `Commit `,
-              },
-            },
-            {
-              type: "text",
-              annotations: {
-                code: true,
-              },
-              text: {
-                content: `${commit.sha.substring(0, 7)}`,
-              },
-            },
-            {
-              type: "text",
-              annotations: {
-                bold: true,
-              },
-              text: {
-                content: `: ${commit.commit.message}`,
-              },
-            },
-          ],
-        },
-      };
-
-      const bookmark = {
-        object: "block",
-        type: "bookmark",
-        bookmark: {
-          url: commit.html_url,
-          caption: [
-            {
-              type: "text",
-              text: {
-                content: `By `,
-              },
-            },
-            {
-              type: "text",
-              text: {
-                content: ` (`,
-              },
-            },
-            {
-              type: "text",
-              text: {
-                content: `${commit.author.login}`,
-                link: {
-                  type: "url",
-                  url: `${commit.author.html_url}`,
-                },
-              },
-            },
-            {
-              type: "text",
-              text: {
-                content: `) on `,
-              },
-            },
-            {
-              type: "mention",
-              mention: {
-                date: {
-                  start: commit.commit.author.date,
-                },
-              },
-            },
-          ],
-        },
-      };
-
-      user
-        ? bookmark.bookmark.caption.splice(1, 0, {
-            type: "mention",
-            mention: {
-              user: {
-                id: user.notionUserId,
-              },
-            },
-          })
-        : bookmark.bookmark.caption.splice(1, 0, {
-            type: "text",
-            text: {
-              content: commit.commit.author.name,
-            },
-          });
-
-      return [paragraph, bookmark];
-    })
-    .flat();
-
-  const [pageChildren, error] = await getChildren(notion);
+  // Read
+  const [children, error] = await getChildren(notion);
   error && exitWithError("Notion", error);
 
-  const targetBlockIdx = pageChildren.results.findIndex((block) => {
-    return (
-      block.type === "heading_2" &&
-      block.heading_2.rich_text[0].plain_text === "Docs"
-    );
-  });
-
-  const [archivedChildren, error2] = await archiveChildren(
-    notion,
-    pageChildren
-  );
+  // Remove
+  const [archivedChildren, error2] = await archiveChildren(notion, children);
   error2 && exitWithError("Notion", error2);
-
   console.log("Archived %s blocks", archivedChildren.length);
 
-  pageChildren.results.splice(targetBlockIdx + 1, 0, ...children);
-
-  const [appendedChildren, error3] = await appendChildren(notion, pageChildren);
+  // Replace
+  const newChildren = createNewChildren(commits, children);
+  const [appendedChildren, error3] = await appendChildren(notion, newChildren);
   error3 && exitWithError("Notion", error3);
 
   return appendedChildren;
